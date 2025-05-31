e0479025d0d6b9fab8a6d10b7d9d0f479d32cf9f
Cyprian <lightningcyranus@gmail.com>
2025-05-31 08:02:45 +0200

    bound an event listener to an element

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const input = document.getElementById('button');
+
 function addingEventListener() {
+  input.addEventListener('click', () => {
+    alert('Button clicked!');
+  });
 }
+// Add the event listener when the script loads
+addingEventListener();
+
